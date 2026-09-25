33bcdbc86065c4c1a94b878e0a3eb67b048316b3
Tomas Ekeli <tomas@eke.li>
2026-09-25 19:31:58 +0000

    Stop checking upstream's releases for updates
    The check polled the FreeTube team's releases, and a newer version there
    put up a banner offering their build, with a download button to their
    site. For anyone running this fork that is a build without any of its
    changes, and a road to the team's own channels with a problem that is
    not theirs.
    
    6c26164a3 turned it off by default and kept the toggle, so it could be
    turned back on. It could only ever be turned back on against upstream,
    though: this fork publishes no releases, only build runs, so there is
    nothing of its own to check against.
    
    So it goes: the fetch, the banner, the release notes prompt, the
    checkForUpdates setting and its toggle in General settings. A stored
    value for the setting is ignored on load, as any unknown key is.
    
    marked was only used for the release notes, and FtNotificationBanner
    only for the banner. Both stay in the tree, unused, so upstream's
    changes to them keep merging cleanly.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -18,17 +18,6 @@
       role="main"
       :inert="isAnyPromptOpen"
     >
-      <div
-        v-if="showUpdatesBanner"
-        class="banner-wrapper"
-      >
-        <FtNotificationBanner
-          class="banner"
-          :message="updateBannerMessage"
-          role="link"
-          @click="handleUpdateBannerClick"
-        />
-      </div>
       <RouterView
         v-slot="{ Component }"
         class="routerView"
@@ -41,39 +30,6 @@
         </Transition>
       </RouterView>
     </FtFlexBox>
-    <FtPrompt
-      v-if="showReleaseNotes"
-      theme="readable-width"
-      @click="toggleShowReleaseNotes"
-    >
-      <template #label="{ labelId }">
-        <h1
-          :id="labelId"
-          class="changeLogTitle"
-          dir="ltr"
-        >
-          {{ changeLogTitle }}
-        </h1>
-      </template>
-      <bdo
-        v-safer-html.lenient="updateChangelog"
-        class="changeLogText"
-        dir="ltr"
-        lang="en"
-      />
-      <FtFlexBox>
-        <FtButton
-          :label="t('Download From Site')"
-          @click="openDownloadsPage"
-        />
-        <FtButton
-          :label="t('Close')"
-          :text-color="null"
-          :background-color="null"
-          @click="toggleShowReleaseNotes"
-        />
-      </FtFlexBox>
-    </FtPrompt>
     <FtPrompt
       v-if="showExternalLinkOpeningPrompt"
       :label="t('Are you sure you want to open this link?')"
@@ -102,7 +58,6 @@
 </template>
 
 <script setup>
-import { marked } from 'marked'
 import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
@@ -110,16 +65,13 @@ import { useRoute, useRouter } from 'vue-router'
 import FtFlexBox from './components/ft-flex-box/ft-flex-box.vue'
 import TopNav from './components/TopNav/TopNav.vue'
 import SideNav from './components/SideNav/SideNav.vue'
-import FtNotificationBanner from './components/FtNotificationBanner/FtNotificationBanner.vue'
 import FtPrompt from './components/FtPrompt/FtPrompt.vue'
-import FtButton from './components/FtButton/FtButton.vue'
 import FtToast from './components/FtToast/FtToast.vue'
 import FtProgressBar from './components/FtProgressBar/FtProgressBar.vue'
 import FtPlaylistAddVideoPrompt from './components/FtPlaylistAddVideoPrompt/FtPlaylistAddVideoPrompt.vue'
 import FtCreatePlaylistPrompt from './components/FtCreatePlaylistPrompt/FtCreatePlaylistPrompt.vue'
 import FtKeyboardShortcutPrompt from './components/FtKeyboardShortcutPrompt/FtKeyboardShortcutPrompt.vue'
 import FtSearchFilters from './components/FtSearchFilters/FtSearchFilters.vue'
-import { vSaferHtml } from './directives/vSaferHtml.js'
 
 import store from './store/index'
 
@@ -196,10 +148,6 @@ onMounted(async () => {
     }
 
     dataReady.value = true
-
-    setTimeout(() => {
-      checkForNewUpdates()
-    }, 500)
   })
 
   if (route.path === '/') {
@@ -257,82 +205,6 @@ function updateDensity() {
 }
 
 updateDensity()
-
-const showUpdatesBanner = ref(false)
-const latestVersionNumber = ref('')
-const showReleaseNotes = ref(false)
-const changeLogTitle = ref('')
-const updateChangelog = ref('')
-
-/** @type {import('vue').ComputedRef<boolean>} */
-const checkForUpdates = computed(() => store.getters.getCheckForUpdates)
-
-const updateBannerMessage = computed(() => {
-  return t('Version {versionNumber} is now available!  Click for more details', {
-    versionNumber: latestVersionNumber.value
-  })
-})
-
-async function checkForNewUpdates() {
-  if (!checkForUpdates.value) {
-    return
-  }
-
-  try {
-    const response = await fetch('https://api.github.com/repos/freetubeapp/freetube/releases?per_page=1')
-    const json = await response.json()
-
-    const tagName = json[0].tag_name
-    const versionNumber = tagName.replace('v', '').replace('-beta', '')
-
-    let changelog = json[0].body
-      // Link usernames to their GitHub profiles
-      .replaceAll(/@(\S+)\b/g, '[@$1](https://github.com/$1)')
-      // Shorten pull request links to #1234
-      .replaceAll(/https:\/\/github\.com\/FreeTubeApp\/FreeTube\/pull\/(\d+)/g, '[#$1]($&)')
-
-    // Add the title
-    changelog = `${changelog}`
-
-    updateChangelog.value = marked.parse(changelog)
-    changeLogTitle.value = json[0].name
-    latestVersionNumber.value = versionNumber
-
-    const appVersion = packageDetails.version.split('.')
-    const latestVersion = versionNumber.split('.')
-
-    if (parseInt(appVersion[0]) < parseInt(latestVersion[0])) {
-      showUpdatesBanner.value = true
-    } else if (parseInt(appVersion[1]) < parseInt(latestVersion[1])) {
-      showUpdatesBanner.value = true
-    } else if (parseInt(appVersion[2]) < parseInt(latestVersion[2]) && parseInt(appVersion[1]) <= parseInt(latestVersion[1])) {
-      showUpdatesBanner.value = true
-    }
-  } catch (error) {
-    console.error('errored while checking for updates', 'https://api.github.com/repos/freetubeapp/freetube/releases?per_page=1', error)
-  }
-}
-
-function toggleShowReleaseNotes() {
-  showReleaseNotes.value = !showReleaseNotes.value
-}
-
-/**
- * @param {boolean} response
- */
-function handleUpdateBannerClick(response) {
-  if (response) {
-    showReleaseNotes.value = true
-  } else {
-    showUpdatesBanner.value = false
-  }
-}
-
-function openDownloadsPage() {
-  openExternalLink('https://freetubeapp.io#download')
-  showReleaseNotes.value = false
-  showUpdatesBanner.value = false
-}
 
 /** @type {import('vue').ComputedRef<boolean>} */
 const outlinesHidden = computed(() => store.getters.getOutlinesHidden)
